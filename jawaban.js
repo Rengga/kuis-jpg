4d9041e7b9409361edecfbb1805653f54610c5ae
Rengga <renggaarnanta@gmail.com>
2024-04-15 20:54:54 +0700

    next

---
+++ jawaban.js
@@ -0,0 +1,14 @@
+//h-1
+var allAnswers = [
+  { romaji: "tsukue", jpg: "つくえ / 机", indo: "meja" },
+  { romaji: "isu", jpg: "いす / 椅子", indo: "kursi" },
+  { romaji: "tana", jpg: "たな / 田な", indo: "ladang" },
+  { romaji: "tana", jpg: "たな / 棚", indo: "lemari" },
+  { romaji: "tana", jpg: "たな / 種", indo: "benih atau jenis" },
+  { romaji: "terebi", jpg: "テレビ", indo: "televisi" },
+  { romaji: "reizouko", jpg: "れいぞうこ / 冷蔵庫", indo: "kulkas" },
+  { romaji: "sentakuki /sentakki", jpg: "せんたくき / 洗濯機", indo: "mesin cuci" },
+  { romaji: "kagami", jpg: "かがみ / 鏡", indo: "cermin" },
+  { romaji: "beddo / shindai", jpg: "ベッド / 寝台", indo: "tempat tidur" },
+  { romaji: "futon", jpg: "ふとん / 布団", indo: "kasur" },
+];
